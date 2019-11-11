eec802132d69572ed19ae6c0c08782ecce914c72
zhou <953605663@qq.com>
2019-11-11 16:53:15 +0800

    订单页面

--- order/src/main/java/com/tianshang/fastman/order/push/PushOrderFragment.java
+++ order/src/main/java/com/tianshang/fastman/order/push/PushOrderFragment.java
@@ -3,11 +3,15 @@ package com.tianshang.fastman.order.push;
 import android.os.Bundle;
 import android.view.View;
 
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.tianshang.common.base.mvp.BaseViewFm;
+import com.tianshang.common.entity.app.TaskListEntity;
 import com.tianshang.fastman.order.R;
 import com.tianshang.fastman.order.R2;
+
+import java.util.ArrayList;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -20,7 +24,7 @@ public class PushOrderFragment extends BaseViewFm<PushOrderFmPresenter, PushOrde
      * 1:待付款2：待认领3：待收件4：待评价5：已取消
      */
     private int type;
-
+    private PushOrderAdapter adapter;
     public PushOrderFragment(int type) {
         this.type = type;
     }
@@ -33,6 +37,31 @@ public class PushOrderFragment extends BaseViewFm<PushOrderFmPresenter, PushOrde
     @Override
     protected void initView(View view, Bundle savedInstanceState) {
         ButterKnife.bind(this, view);
+
+        LinearLayoutManager manager = new LinearLayoutManager(getContext());
+        manager.setOrientation(RecyclerView.VERTICAL);
+        rvOrder.setLayoutManager(manager);
+        
+        ArrayList<String> list = new ArrayList<>();
+        list.add("到8栋天天快递取件");
+        list.add("到23栋中通快递取件");
+        list.add("到19栋申通快递取件");
+        list.add("到5栋EMS快递取件");
+        list.add("到12栋圆通快递取件");
+        list.add("到三食堂买大份黄焖鸡");
+        list.add("到8栋天天快递取件");
+        list.add("到23栋中通快递取件");
+        list.add("到19栋申通快递取件");
+        list.add("到5栋EMS快递取件");
+        list.add("到12栋圆通快递取件");
+        list.add("到三食堂买大份黄焖鸡");
+        list.add("到8栋天天快递取件");
+        list.add("到23栋中通快递取件");
+        list.add("到19栋申通快递取件");
+        list.add("到5栋EMS快递取件");
+        list.add("到12栋圆通快递取件");
+        list.add("到三食堂买大份黄焖鸡");
+        adapter = new PushOrderAdapter(list);
     }
 
 
